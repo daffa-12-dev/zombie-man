586da381aa1f0faa0aa970594b1032d64c44729a
Daffa Bayu <daffabayu011205@gmail.com>
2025-06-01 20:39:11 +0700

    html to php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Zombie vs Man</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Zombie vs Man</h1>
+  <div id="timer">⏳ Timer: 30</div>
+  <div id="score">Skor: 0 | Level: 1</div>
+  <button id="startBtn">Start Game</button>
+  <button id="nextBtn" style="display: none;">Next Level</button>
+  <div id="grid"></div>
+  <script src="script.js"></script>
+</body>
+</html>
